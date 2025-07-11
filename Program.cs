1ee0dd6842731ab2bf351949a88a85dc46df0c43
Hassan Allam <hassan309pro@gmail.com>
2025-07-11 18:31:44 +0300

    Q1 : Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace Assignment_OOP_2
 {
+    #region Struct Q1
+    internal struct Person
+    {
+        public string Name;
+        public int Age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    #endregion
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Person[] people = new Person[3];
+            //people[0] = new Person("Hassan", 20);
+            //people[1] = new Person("Mohamed", 24);
+            //people[2] = new Person("Ahmed", 16);
+            //Console.WriteLine("Person Details : ");
+            //
+            //foreach (Person person in people)
+            //{
+            //    Console.WriteLine($"Name : {person.Name}, Age: {person.Age}");
+            //}
+            #endregion
         }
     }
 }
